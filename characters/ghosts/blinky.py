5424925318162d8fada35dc96e4f3b480e25fda2
quyen29 <maimyquyen.32011@gmail.com>
2025-05-04 10:06:08 +0700

    Sua loi cu phap

--- characters/ghosts/blinky.py
+++ characters/ghosts/blinky.py
@@ -9,11 +9,10 @@ class Blinky(Agent):
         super().__init__(index)
         self.mode = 'chase'
         self.prng = PRNG(seed=12345)
-        self.scattertarget = (27, 0)
+        self.scattertarget = (27, 0) # Quay về góc trên phải
 
-    def getAction(self, state=GameState):
+    def getAction(self, state: GameState):
         blinky_state = state.getGhostState(self.index)
-        blinky_pos = blinky_state.getPosition()
         pacman_pos = state.getPacmanPosition()
 
         if blinky_state.scaredTimer > 0:
@@ -23,8 +22,8 @@ class Blinky(Agent):
                 return Directions.STOP
             
             direction_priority = [Directions.NORTH, Directions.WEST, Directions.SOUTH, Directions.EAST]
-            seed_index = self.prng.next() % len(direction_priority)
-            preferred = direction_priority[seed_index]
+            choice = self.prng.next() % len(direction_priority)
+            preferred = direction_priority[choice]
 
             if preferred in legal:
                 return preferred
@@ -35,24 +34,22 @@ class Blinky(Agent):
     
         if hasattr(state, 'isInChaseMode') and state.isInChaseMode():
             target = pacman_pos
-        elif hasattr(state, 'isInScatterMode') and state.isInScatterMode():
-            target = self.scattertarget
         else:
             target = self.scattertarget
         
-        problem = BlinkySearchProblem(state, goal=target, ghost_index=self.index)
+        problem = BlinkySearchProblem(state, target, self.index)
         path = pacmanASS(problem, heuristic=lambda pos, _: manhattanDistance(pos, target))
         if path:
             return path[0]
         return Directions.STOP
 
 class BlinkySearchProblem():
-    def __init__(self, goal, game_state, ghost_index=1):
-        self.start = game_state.getGhostState(ghost_index).getPosition()
+    def __init__(self, state: GameState, goal, ghost_index=1):
+        self.start = state.getGhostState(ghost_index).getPosition()
         self.goal = goal
-        self.game_state = game_state
         self.ghost_index = ghost_index
-        self.walls = game_state.getWalls()
+        self.walls = state.getWalls()
+        self.directions = [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]
 
     def getStartState(self):
         return self.start
@@ -62,10 +59,10 @@ class BlinkySearchProblem():
     
     def getSuccessors(self, state):
         successors = []
-        for action in Directions.DIRECTIONS:
+        for action in self.directions:
             dx, dy = Actions.directionToVector(action)
             next_x, next_y = int(state[0] + dx), int(state[1] + dy)
-            if not self.walls[next_x][next_y]:
+            if 0 <= next_x < self.walls.getWidth() and 0 <= next_y < self.walls.getHeight() and not self.walls[next_x][next_y]:
                 successors.append(((next_x, next_y), action, 1))
             
         return successors
