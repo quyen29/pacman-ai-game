a68977b98aa70c138b3d745dd23f80839da54645
dieuhang20404 <dieuhang20404@example.com>
2025-05-10 08:59:20 +0700

    bỏ vòng while bị dư

--- characters/ghosts/pinky.py
+++ characters/ghosts/pinky.py
@@ -20,12 +20,6 @@ class Pinky(Agent):
         if mode ==Modes.CHASE:
             pacmanDir = state.data.agentStates[0].configuration.direction
             predicted_target = Actions.get_ahead_position(pacmanPos, pacmanDir, 4)
-            x, y = predicted_target
-            if (walls[x][y]==True):
-                i = 3
-                while(walls[x][y]!=True):
-                    predicted_target = Actions.get_ahead_position(pacmanPos, pacmanDir, i)
-                    x, y = predicted_target
             target = predicted_target if manhattanDistance(currentPos, predicted_target) > manhattanDistance(currentPos, pacmanPos) else pacmanPos
 
         elif mode == Modes.SCATTER:
